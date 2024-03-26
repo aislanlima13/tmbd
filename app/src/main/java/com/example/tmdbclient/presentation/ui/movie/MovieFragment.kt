dfa352aefa1d4dc25c2f94ec95f6a07fd5e00073
aislanlima13 <aislan.martins99@gmail.com>
2024-03-26 09:29:17 -0300

    Create movie presentation

--- app/src/main/java/com/example/tmdbclient/presentation/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/example/tmdbclient/presentation/ui/movie/MovieFragment.kt
@@ -1,53 +1,38 @@
 package com.example.tmdbclient.presentation.ui.movie
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
-import com.example.tmdbclient.databinding.FragmentHomeBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tmdbclient.databinding.FragmentMovieBinding
 import com.example.tmdbclient.presentation.di.Injector
 import javax.inject.Inject
 
 class MovieFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentMovieBinding? = null
     private val binding get() = _binding!!
 
     @Inject
     lateinit var factory: MovieViewModelFactory
     private lateinit var viewModel: MovieViewModel
+    private lateinit var adapter: MovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val movieViewModel =
-            ViewModelProvider(this).get(MovieViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentMovieBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        movieViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-
-        (context as Injector).createMovieSubComponent()
-            .inject(this)
-
-        viewModel = ViewModelProvider(this, factory)
-            .get(MovieViewModel::class.java)
-
-        viewModel.getMovies()
+        initViewModel()
+        initRecyclerView()
+        initObserver()
 
         return root
     }
@@ -55,5 +40,34 @@ class MovieFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initViewModel() {
+        (viewLifecycleOwner as Injector).createMovieSubComponent()
+            .inject(this)
+
+        viewModel = ViewModelProvider(this, factory)
+            .get(MovieViewModel::class.java)
+    }
+
+    private fun initRecyclerView() {
+        binding.rvMovies.layoutManager = LinearLayoutManager(context)
+        adapter = MovieAdapter()
+        binding.rvMovies.adapter = adapter
+    }
+
+    private fun initObserver() {
+        binding.pbMovie.visibility = View.VISIBLE
+        val responseLiveData = viewModel.getMovies()
+        responseLiveData.observe(viewLifecycleOwner) {
+            if (!it.isNullOrEmpty()) {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.pbMovie.visibility = View.GONE
+            } else {
+                binding.pbMovie.visibility = View.GONE
+                Toast.makeText(context, "No data available", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
